34499067467240bc54f99b2851174ba05f6e9b3a
suntkentay@gmail.com <suntkentay@gmail.com>
2026-05-27 08:23:28 -0500

    sign-up

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export const Header = () => {
     return (
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            Header
+            <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src="/duolingo-1.svg" alt="Duolingo Logo" width={40} height={40} />
+                    <h1>Lingo</h1>
+                </div>
+            </div>
         </header>
     );
 };
